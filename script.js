3ee8caa2c83faf0f638c0b5a45ce6287e1fc5857
jesusoyer <oyervidesjesus017@gmail.com>
2022-05-05 14:22:14 -0500

    new porfolio media queries

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
-let bio = document.querySelector('#bioText')
-let jimage = document.querySelector("#jesuspic")
-let greeting = document.querySelector("#greeting")
-let myName = document.querySelector('#myName')
+const header = document.querySelector('#header')
+const intro = document.querySelector('#intro')
+const skills = document.querySelector('#skills')
 
 
-
-
-function greetingUser(){
-    greeting.textContent = "Hello!"
-}
-function introduction(){
-    myName.textContent="My name is Jesus Oyervides Jr"
-}
-
-// jimage.addEventListener('click', displayBio)
-function displayBio(){
-    bio.textContent = 'I am a student in the UT full stack part-time coding bootcamp. I am originally from West Texas, where the only thing you see is pump jacks and cotton fields for miles. I love creating and building things, whether it be model kits, legos, trading card game decks, or websites. My passion to create has no bounds! I would say my goal is to become the very best software engineer like no one ever was! To learn all the programming languages is my real test! To utilize them is my cause!'
-}
-
-
-window.setTimeout(greetingUser,3000)
-window.setTimeout(introduction,5000)
-window.setTimeout(displayBio,7000)
+header.textContent = "My Portfolio"
+intro.textContent = "My name is Jesus Oyervides jr, I am currently a student in the UT full stack software development bootcamp. Welcome to my portfolio page! I am excited to show you what projects I have worked on."
+skills.textContent="The current languages I know thus far are, HTML, CSS and Javascript. I am currently learning Node.js and will learn many more. Please explore my page to see what I've worked on."
